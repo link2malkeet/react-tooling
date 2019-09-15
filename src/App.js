7fe0253d1c13b128843c78ee97ccf316e50c68c7
Malkeet Singh <link2malkeet@gmail.com>
2019-09-15 18:06:26 +1000

    added plugin-proposal-class-properties to understand static and class properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {
+        count: 0
+    }
     render() {
-        return <h1>Hello React Tooling</h1>
+        return (
+            <div>
+                <h1>Hello React Tooling - {this.state.count}</h1>
+                <button onClick={() => this.setState({ count: ++this.state.count })}>+</button>
+                <button onClick={() => this.setState({ count: --this.state.count })}>-</button>
+            </div>
+        )
     }
 }
 
